79790b21c4296851686cda4f0ba31ad24bef865f
Asger F <asgerf@github.com>
2022-10-21 12:54:05 +0200

    Ruby: add similar test but inside a module

--- ruby/ql/test/library-tests/modules/unresolved_subclass.rb
+++ ruby/ql/test/library-tests/modules/unresolved_subclass.rb
@@ -6,3 +6,10 @@ end
 
 class UnresolvedNamespace::Subclass2 < UnresolvedNamespace::Subclass1
 end
+
+module Foo
+    class UnresolvedInsideFoo::Class1
+    end
+    class UnresolvedInsideFoo::Class2 < UnresolvedInsideFoo::Class1
+    end
+end
