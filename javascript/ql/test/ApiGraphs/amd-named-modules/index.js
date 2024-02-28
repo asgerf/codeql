0f3f5e4ce709b857bb4e64981d42c8fadd6ac402
Asger F <asgerf@github.com>
2024-02-28 16:08:39 +0100

    JS: Add FN test with 'exports' variable

--- javascript/ql/test/ApiGraphs/amd-named-modules/index.js
+++ javascript/ql/test/ApiGraphs/amd-named-modules/index.js
@@ -6,8 +6,13 @@ define('amd-named-modules/lib', [], function () {
     };
 });
 
-define('amd-named-modules', ['amd-named-modules/lib'], function(foo) {
+define('amd-named-modules/lib2', ['exports'], function (exports) {
+    exports.exportedVar = 123; // def=moduleImport("amd-named-modules").getMember("exports").getMember("lib2").getMember("exportedVar")
+});
+
+define('amd-named-modules', ['amd-named-modules/lib', 'amd-named-modules/lib2'], function(foo, lib2) {
     return {
-        bar: foo.foo // def=moduleImport("amd-named-modules").getMember("exports").getMember("bar")
+        bar: foo.foo, // def=moduleImport("amd-named-modules").getMember("exports").getMember("bar")
+        lib2,
     }
 });
