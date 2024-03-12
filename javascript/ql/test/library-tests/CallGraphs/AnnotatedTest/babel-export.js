fae1fa30e7a88dcff59c4415cb5cfc11e8f1e44f
Asger F <asgerf@github.com>
2024-03-12 14:06:00 +0100

    JS: Add babel interop test case

---
+++ javascript/ql/test/library-tests/CallGraphs/AnnotatedTest/babel-export.js
@@ -0,0 +1,11 @@
+exports.__esModule = true;
+
+exports.default = class DefaultExportedClass {
+  /** name:DefaultExportedClass.m */
+  m() {}
+}
+
+exports.Named = class NamedExportedClass {
+  /** name:NamedExportedClass.m */
+  m() {}
+}
---
+++ javascript/ql/test/library-tests/CallGraphs/AnnotatedTest/babel-import-interop.js
@@ -0,0 +1,71 @@
+function _interopRequireDefault(obj) {
+    return obj && obj.__esModule ? obj : { default: obj };
+}
+
+function _interopRequireWildcard(obj) {
+    if (obj && obj.__esModule) {
+      return obj;
+    } else {
+      var newObj = {};
+      if (obj != null) {
+        for (var key in obj) {
+          if (Object.prototype.hasOwnProperty.call(obj, key))
+            newObj[key] = obj[key];
+        }
+      }
+      newObj["default"] = obj;
+      return newObj;
+    }
+}
+
+function _interopDefault(ex) {
+    return ex && typeof ex === "object" && "default" in ex ? ex["default"] : ex;
+}
+
+//
+// Plain import
+//
+const ed = require('./babel-export');
+const defaultInstance1 = new ed.default();
+/** calls:DefaultExportedClass.m */
+defaultInstance1.m();
+
+const namedInstance1 = new ed.Named();
+/** calls:NamedExportedClass.m */
+namedInstance1.m();
+
+//
+// _interopRequireDefault
+//
+const ed2 = _interopRequireDefault(ed);
+const defaultInstance2 = new ed2.default();
+/** calls:DefaultExportedClass.m */
+defaultInstance2.m();
+
+const namedInstance2 = new ed2.Named();
+/** calls:NamedExportedClass.m */
+namedInstance2.m();
+
+//
+// _interopRequireWildcard
+//
+const ed3 = _interopRequireWildcard(ed);
+const defaultInstance3 = new ed3.default();
+/** calls:DefaultExportedClass.m */
+defaultInstance3.m();
+
+const namedInstance3 = new ed3.Named();
+/** calls:NamedExportedClass.m */
+namedInstance3.m();
+
+//
+// _interopDefault
+//
+const ed4 = _interopDefault(ed);
+const defaultInstance4 = new ed4();
+/** calls:DefaultExportedClass.m */
+defaultInstance4.m();
+
+const namedInstance4 = new ed4.Named();
+/** calls:NONE */
+namedInstance4.m();
