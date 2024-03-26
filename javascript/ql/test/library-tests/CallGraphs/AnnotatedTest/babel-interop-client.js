31274caa700136ad75f9b4ece564ad9006f72aa3
Asger F <asgerf@github.com>
2024-03-26 10:47:08 +0100

    JS: Add another test case

---
+++ javascript/ql/test/library-tests/CallGraphs/AnnotatedTest/babel-interop-client.js
@@ -0,0 +1,14 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _lib = require("./babel-interop-lib");
+
+var _lib2 = _interopRequireDefault(_lib);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+/** calls:babel.interop.func */
+(0, _lib2.default)();
