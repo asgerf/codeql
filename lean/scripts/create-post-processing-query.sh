010ebac3b814bf2fb2ec09f62845f8cbe95f93c2
Asger F <asgerf@github.com>
2025-05-15 15:13:36 +0200

    Fix upgrade script generator

--- lean/scripts/create-post-processing-query.sh
+++ lean/scripts/create-post-processing-query.sh
@@ -6,14 +6,22 @@ SCRIPT_DIR=$(dirname "$(realpath "$0")")
 ROOT_DIR="$SCRIPT_DIR/.."
 
 function strip_imports() {
-    sed -E '/^ *(private )?import (codeql|All|MakeLanguageBase)/d'
+    sed -E '/^ *(private )?import (codeql|MakeLanguageBase)/d'
 }
 function strip_signature() {
     sed -E 's/implements LanguageBaseSig<Location>//'
 }
 
-cat \
-    "$ROOT_DIR"/ql/lib/codeql/js/base/*.qll \
-    "$ROOT_DIR"/ql/lib/upgrades/post-process/PostProcess.ql \
-    | strip_imports \
-    | strip_signature
+function wrap_file() {
+    ModuleName=$(basename "$1" .ql)
+    echo "module $ModuleName {"
+    cat "$1"
+    echo "}"
+}
+
+{
+    for file in "$ROOT_DIR"/ql/lib/codeql/js/base/*.qll; do
+        wrap_file "$file"
+    done
+    cat "$ROOT_DIR"/ql/lib/upgrades/post-process/PostProcess.ql
+} | strip_imports | strip_signature
